c028db8d2fe8edcbc27105a30e471209669cbc00
0/0 <zero@imaskeleton.me>
2022-01-28 12:07:36 -0700

    remove useless tests

--- nbb_markdown/src/lib.rs
+++ nbb_markdown/src/lib.rs
@@ -5,12 +5,3 @@ mod cfg;
 mod render;
 
 pub use render::render_markdown;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
