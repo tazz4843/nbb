fa87b66479edec2e1174e7a7935d657f0c74f609
Niko <niko@niko.lgbt>
2025-05-02 16:25:37 -0700

    Remove pointless identity

--- nbb_markdown/src/render.rs
+++ nbb_markdown/src/render.rs
@@ -1,4 +1,3 @@
-use std::convert::identity;
 use std::path::Path;
 
 /// Render Markdown to HTML.
@@ -12,13 +11,10 @@ use std::path::Path;
 pub fn render_markdown(path: &Path) -> String {
     let cfg = crate::cfg::get_config();
 
-    crate::cache::get_from_cache(path).map_or_else(
-        || {
-            let md = std::fs::read_to_string(path).expect("file doesn't exist!");
-            let rendered = comrak::markdown_to_html(&md, cfg);
-            crate::cache::insert_into_cache(path.to_path_buf(), rendered.clone());
-            rendered
-        },
-        identity,
-    )
+    crate::cache::get_from_cache(path).unwrap_or_else(|| {
+        let md = std::fs::read_to_string(path).expect("file doesn't exist!");
+        let rendered = comrak::markdown_to_html(&md, cfg);
+        crate::cache::insert_into_cache(path.to_path_buf(), rendered.clone());
+        rendered
+    })
 }
